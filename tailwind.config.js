176087c8f0c905e8d923fc581fbfec5b86539d1e
kshitij <kshitij_987@protonmail.com>
2023-03-13 23:37:52 +0530

    Added tailwind css to inital project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  ontent: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   content: [],
   theme: {
     extend: {},
